4f1253c190c256ecdc095e294136c2c52e029218
shoaibalirts <shoaibali.rts@gmail.com>
2025-11-11 18:24:01 +0100

    applied Footer

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -1,6 +1,26 @@
+<template>
+  <v-footer class="text-center d-flex flex-column ga-2 py-4 bg-primary" color="indigo-lighten-1">
+    <div class="d-flex ga-3">
+      <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        :icon="icon"
+        density="comfortable"
+        variant="text"
+      ></v-btn>
+    </div>
+
+    <v-divider class="my-2" thickness="2" width="50"></v-divider>
+  </v-footer>
+</template>
 <script>
 export default {
-  data() {},
+  data() {
+    return {
+      // Define the array of icon names here
+      icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
+    };
+  },
   methods: {},
 };
 </script>
